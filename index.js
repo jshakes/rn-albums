387cc66ff8e37301a3d3df21fe5845313a263769
James Shakespeare <j@jshakespeare.com>
2017-12-09 15:26:50 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+const App = () => (
+  <Text>Some text</Text>
+);
+
+AppRegistry.registerComponent('albums', () => App);
